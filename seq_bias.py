51d04d41917f4ddd060ec4f93adc747912bb8a8e
Jordan Gewing-Mullins <131607800+jordangewing-mullins@users.noreply.github.com>
2024-01-24 20:02:47 -0800

    Add files via upload

--- seq_bias.py
+++ seq_bias.py
@@ -1,3 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+# ---
+
 # piip install pandas openpyxl shap xgboost
 import pandas as pd
 import openpyxl
